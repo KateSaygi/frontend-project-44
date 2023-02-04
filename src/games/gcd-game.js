0651f84a592d04d088e452ae7a85c0fd5d28371b
Екатерина Сайгы <katesaygi@gmail.com>
2023-02-04 12:58:06 +0300

    gcd change

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -4,26 +4,20 @@ const gameRule = 'Find the greatest common divisor of given numbers.';
 const oneSessionResult = () => {
   const getRandomNum = Math.floor(Math.random() * 10);
   const getRandomNum2 = Math.floor(Math.random() * 10);
-  let a = getRandomNum;
-  let b = getRandomNum2;
-  const question = `${a} ${b}`;
-  let result;
-  const calc = () => {
-    if (a === b) {
-      result = a;
-    }
-    while (a !== b) {
-      if (a > b) {
-        a -= b;
-      } else {
-        b -= a;
-      } result = a;
-    } return result;
-  };
-  const calcResult = `${calc(result)}`;
-  return [question, calcResult];
-};
 
+  const getGcd = (a, b) => {
+    if (b !== 0) {
+      const c = a % b;
+      return getGcd(b, c);
+    } return a;
+  };
+
+  const a = getRandomNum;
+  const b = getRandomNum2;
+  const question = `${a} ${b}`;
+  const correctAnswer = String(getGcd(a, b));
+  return [question, correctAnswer];
+};
 const playGcd = () => {
   mainLogic(gameRule, oneSessionResult);
 };
